e12f8a0111af97a350e07df2e741b0e4b33365ce
alexlamsl <alexlamsl@gmail.com>
2013-04-24 20:20:19 +0100

    fix and upgrade API

--- examples/wot.js
+++ examples/wot.js
@@ -2,24 +2,36 @@
 var HttpClient = require("httpclient").HttpClient;
 
 function get(url) {
-  return new HttpClient({ url: app.url }).finish().body.read();
+  return new HttpClient({
+    url:url,
+    headers:{
+      'User-Agent':userAgent || 'Mozilla/5.0 (Linux) AppleWebKit/537.22 (KHTML, like Gecko)'
+    },
+    timeout:15000,
+    agent:false
+  }).finish();
 }
 
-var extract = module.exports = function(app) {
-  var result = JSON.parse(get("http://api.mywot.com/0.4/public_link_json?hosts=" + encodeURIComponent(app.url) + "/").content);
-  for (var key in result) {
-    result = result[key];
-    break;
-  }
-  if (result && result[0]) {
-    return {
-      trust:result[0] ? result[0][0] : undefined,
-      reliability:result[1] ? result[1][0] : undefined,
-      privacy:result[2] ? result[2][0] : undefined
-    };
+function queryUrl(key, url) {
+  return 'http://api.mywot.com/0.4/public_link_json2?key=' + key + '&hosts=' + encodeURIComponent(url) + '/';
+}
+
+var extract = module.exports = function(app, key) {
+  var response = get(queryUrl(key, app.url));
+  var reply = JSON.parse(response.body.read().decodeToString());
+  response.body.close();
+  for (var key in reply) {
+    var data = reply[key];
+    var result = {};
+    ['trust', 'reliability', 'privacy'].forEach(function(name, index) {
+      if (data[index][0]) {
+        result[name] = data[index][0];
+      }
+    });
+    return result;
   }
 };
 
 if (require.main === module) {
-  console.log(extract({url:process.argv[2]}));
+  console.log(extract({url:process.argv[2]}, 'Your API key'));
 }
