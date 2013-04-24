c35503922fb92a662c364aa3d377a9737931c085
alexlamsl <alexlamsl@gmail.com>
2013-04-24 20:22:12 +0100

    fix script

--- index.js
+++ index.js
@@ -2,9 +2,9 @@
 var HttpClient = require("httpclient").HttpClient;
 
 var extract = module.exports = function(app) {
-  return new HttpClient({ url: app.url }).finish().body.read().length;
+  return new HttpClient({url:app.url}).finish().body.read().length;
 };
 
 if (require.main === module) {
-  console.log(extract({name: url:process.argv[2], url:process.argv[3]}));
+  console.log(extract({name:process.argv[2], url:process.argv[3]}));
 }
